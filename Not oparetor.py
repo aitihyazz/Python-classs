c262aa3d5912ef93fc85a8411503a3cba8455b7e
aitihyazz <aitihyazamadder@gmail.com>
2024-12-27 13:03:26 +0600

    Not oparetor

---
+++ Not oparetor.py
@@ -0,0 +1,19 @@
+a=10
+b=13
+c=13
+print (a!= b)
+print (b!=c)
+
+a= "Python"
+b= "coding"
+if a !=b:
+    print(a,"and",b,"are not same" )
+
+a=4
+b=5
+if(a==1)!=(b==5):
+    print("hello")
+
+a=int(input("type a number:"))  
+if a%2 !=0:
+    print(a,"is not a even number")
