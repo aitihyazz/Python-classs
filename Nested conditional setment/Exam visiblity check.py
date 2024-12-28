5614e588a1f192213c709d65e26b9e9c6e21ab3b
aitihyazz <aitihyasamadder@gmail.com>
2024-12-28 22:36:44 +0600

    exam

--- Nested conditional setment/Exam visiblity check.py
+++ Nested conditional setment/Exam visiblity check.py
@@ -1,1 +1,10 @@
-medical c
+medical_cause=input("Did you have a medical cause Y or N:")
+attend=input("enter the attenedance of the student")
+if medical_cause == "Y":
+    print("You are allowed")
+else:   
+ if attend>=75:
+    print ("You are allowed")    
+ else:
+    print("Not allowed")
+
